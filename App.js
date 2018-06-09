f18c69084358441f63383513d734fc2355f8d527
madhurisarkar04 <40093788+madhurisarkar04@users.noreply.github.com>
2018-06-09 12:57:05 +0530

    added app

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import AppStart from './app/start';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text> */}
+        {/* <Home/> */}
+        <AppStart />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
