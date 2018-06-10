081ed84f994e3390ffd02536ec61577557ecc7fa
Madhuri  Sarkar <madhuri.sarkar@Technovert.com>
2018-06-10 09:13:32 +0530

    Updated Event Detail Page

--- app/eventDetails.js
+++ app/eventDetails.js
@@ -12,7 +12,10 @@ import {
     Text,
     View,
     Image,
-    ImageBackground
+    ImageBackground,
+    ScrollView,
+    FlatList,
+    TouchableNativeFeedback
 } from 'react-native';
 import Search from 'react-native-search-box';
 
@@ -31,56 +34,96 @@ export default class EventDetailScreen extends React.Component {
             startDate: "2018-06-09T00:00:00",
             endDate: "2018-06-09T00:00:00",
             isBillable: "true",
-            attendees: "UserA, UserB, UserC, UserD, UserE",
+            attendees: [{
+                id: 1,
+                name: "Venkatesh",
+                email: 'venkatesh@gmail.com',
+                phoneNumber: '9190405060'
+            }, {
+                id: 2,
+                name: "Jyothi",
+                email: 'jyothi@gmail.com',
+                phoneNumber: '9190405061'
+            }, {
+                id: 3,
+                name: "Jyoti",
+                email: 'jyoti@gmail.com',
+                phoneNumber: '9190405063'
+            }, {
+                id: 4,
+                name: "Pavan",
+                email: 'pavan@gmail.com',
+                phoneNumber: '9190405160'
+            }, {
+                id: 5,
+                name: "Umesh",
+                email: 'umesh@gmail.com',
+                phoneNumber: '9190405360'
+            }, {
+                id: 6,
+                name: "Madhuri",
+                email: 'madhuri@gmail.com',
+                phoneNumber: '9198405060'
+            }],
         };
-        var attendees = eventDetails.attendees.split(',');
+
         var startDate = new Date(eventDetails.startDate);
         var endDate = new Date(eventDetails.endDate);
         return (
+            <ScrollView>
+                <View style={styles.container}>
 
-            <View style={styles.container}>
 
+                    <ImageBackground style={styles.eventContainer} source={require('./images/eventBackImg1.jpg')}>
+                        <View style={styles.eventHeaderContainer}>
+                            <View style={styles.headerRule}>
 
-                <ImageBackground style={styles.eventContainer} source={require('./images/eventBackImg1.jpg')}>
-                    <View style={styles.eventHeaderContainer}>
-                        <View style={styles.headerRule}>
-
-                            <Text style={styles.eventHeaderText}>
-                                <Image source={require("./images/events.png")} style={styles.eventImg} /> {eventDetails.eventName}
+                                <Text style={styles.eventHeaderText}>
+                                    <Image source={require("./images/events.png")} style={styles.eventImg} /> {eventDetails.eventName}
+                                </Text>
+                            </View>
+                            <Text style={styles.eventLocationText}>
+                                {eventDetails.eventLocation}
                             </Text>
                         </View>
-                        <Text style={styles.eventLocationText}>
-                            {eventDetails.eventLocation}
+                    </ImageBackground>
+                    <View style={styles.eventDateContainer}>
+                        <Text style={styles.eventDate}>
+                            {startDate.getUTCMonth()}/{startDate.getUTCDate()}/{startDate.getUTCFullYear()} - {endDate.getUTCMonth()}/{endDate.getUTCDate()}/{endDate.getUTCFullYear()}
                         </Text>
                     </View>
-                </ImageBackground>
-                <View style={styles.eventDateContainer}>
-                    <Text >
-                        {startDate.getUTCMonth()}/{startDate.getUTCDate()}/{startDate.getUTCFullYear()} - {endDate.getUTCMonth()}/{endDate.getUTCDate()}/{endDate.getUTCFullYear()}
-                    </Text>
-                </View>
 
-                <View>
-                    <Text>
-                        {eventDetails.description}
-                    </Text>
-                    {attendees.map((attendee) => {
-                        return <Image
-                            style={{ width: 25, height: 25 }}
-                            source={require('./images/user.png')}
-                        />
-                    })}
-                </View>
-                <View>
-                    {attendees.map((attendee) => {
-                        return <Image
-                            style={{ width: 25, height: 25 }}
-                            source={require('./images/user.png')}
-                        />
-                    })}
-                </View>
+                    <View style={styles.eventDescriptionContainer}>
+                        <Text style={[styles.itemCss, styles.descriptionCss]}>
+                            {eventDetails.description}
+                        </Text>
+                    </View>
+                    <View style={[styles.eventDescriptionContainer, styles.userCss]}>
+                        <View style={styles.itemCss}>
+                            {/* {eventDetails.attendees.map((attendee) => {
+                                return <Image
+                                    style={{ width: 25, height: 25 }}
+                                    source={require('./images/user.png')}
+                                />
+                            })} */}
+                            <FlatList
+                                data={eventDetails.attendees}
+                                renderItem={({ item }) => <View>
+                                    <TouchableNativeFeedback>
+                                        <View style={styles.userItemCss} >
+                                            <Image style={styles.itemImg} source={require('./images/user.png')}></Image>
+                                            <View style={styles.itemContent}>
+                                                <Text style={styles.name}>{item.name}</Text>
+                                            </View>
+                                        </View>
+                                    </TouchableNativeFeedback>
+                                </View>}
+                            />
+                        </View>
+                    </View>
 
-            </View>
+                </View>
+            </ScrollView>
         );
     }
 }
@@ -124,10 +167,63 @@ const styles = StyleSheet.create({
         borderBottomColor: '#ffffff',
         borderBottomWidth: 1,
     },
-    eventDateContainer:{
-        padding:20,
-        height:50,
-        backgroundColor:"#ffffff",
-    }
+    eventDateContainer: {
+        padding: 15,
+        paddingLeft: 20,
+        backgroundColor: "#5fba7de6",
+
+    },
+    eventDate: {
+        fontSize: 20,
+        color: "#ffffff"
+    },
+    eventDescription: {
+        fontSize: 15,
+        borderBottomColor: "gray",
+        borderBottomWidth: 1,
+    },
+    eventDescriptionContainer: {
+        padding: 20,
+        paddingBottom: 10,
+        backgroundColor: '#e0e0e0',
+    },
+    itemCss: {
+        backgroundColor: '#ffffff',
+        // flexDirection: 'row',
+        // flexWrap: 'wrap',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        //borderRadius: 10,
+    },
+    descriptionCss: {
+        padding: 10
+    },
+    userContainer: {
+
+    },
+    userCss: {
+        paddingTop: 0
+    },
+    userItemCss: {
+        backgroundColor: '#ffffff',
+        height: 50,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingLeft: 20,
+        borderBottomColor: "#e0e0e0",
+        borderBottomWidth: 1
+    },
+    itemImg: {
+        width: 30,
+        height: 30
+    },
+    itemContent: {
+        height: 50,
+        flex: 3,
+        paddingLeft: 30,
+        justifyContent: 'center'
+    },
 });
 
